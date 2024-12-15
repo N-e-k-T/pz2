5c6f5ef125ee2eea5f296bde2d870d29efbdca1e
N-e-k-T <shootnik022@yandex.ru>
2024-12-15 20:58:41 +0300

    5-й пункт

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 
 int add(int a, int b); // объявление функции add
+int subtract(int a, int b); //объявление функции subtract
 
 int main() {
   // Тест 1: Проверка сложения положительных чисел
@@ -15,7 +16,19 @@ int main() {
     // Тест 3: Проверка сложения отрицательных чисел
     assert(add(-2, -3) == -5);
     std::cout << "Test 3 passed: add(-2, -3) == -5" << std::endl;
-        
+
+     // Тест 4: Проверка вычитания положительных чисел
+    assert(subtract(5, 2) == 3);
+    std::cout << "Test 4 passed: subtract(5, 2) == 3" << std::endl;
+
+    // Тест 5: Проверка вычитания с нулем
+    assert(subtract(5, 0) == 5);
+    std::cout << "Test 5 passed: subtract(5, 0) == 5" << std::endl;
+
+    // Тест 6: Проверка вычитания отрицательных чисел
+    assert(subtract(-5, -2) == -3);
+    std::cout << "Test 6 passed: subtract(-5, -2) == -3" << std::endl;
+
     std::cout << "All tests passed!" << std::endl;
 
     return 0;
